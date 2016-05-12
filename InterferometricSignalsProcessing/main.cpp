4cbbf3701209d2fde4881ff70caed97b43096891
NeoPhix <petr-ermolaev@hotmail.com>
2016-05-12 16:08:39 +0300

    Some tests were made

--- InterferometricSignalsProcessing/main.cpp
+++ InterferometricSignalsProcessing/main.cpp
@@ -27,19 +27,29 @@ int main(int argc, char **argv)
 	for (int i = 0; i < argc; i++)
 		std::cout << argv[i] << std::endl ;
 
-	Eigen::Matrix2d m ;
-	Eigen::Matrix2d *m2 = &m ;
+	Eigen::Matrix2d *m2 = new Eigen::Matrix2d;
+	Eigen::Matrix2d &m = *m2 ;
 	
 	time_t time = clock() ;
 
-	for (long long j = 0; j < 10000000000000000000; j++)
+	for (long long j = 0; j < 10000; j++)
 	{
-		for (long long i = 0; i < 10000000000000000000; i++)
+		for (long long i = 0; i < 10000; i++)
 		{
-			m(0, 0) = i*j;
-			m(0, 1) = i;
-			m(1, 0) = i;
-			m(1, 1) = i;
+			if (i % 2 == 1)
+			{
+				m(0, 0) += i*j;
+				m(0, 1) += i;
+				m(1, 0) += i;
+				m(1, 1) += i;
+			}
+			else
+			{
+				m(0, 0) += j;
+				m(0, 1) += j;
+				m(1, 0) += j;
+				m(1, 1) += j;
+			}
 		}
 	}
 	
@@ -47,14 +57,24 @@ int main(int argc, char **argv)
 
 	time = clock();
 
-	for (long long j = 0; j < 10000000000000000000; j++)
+	for (long long j = 0; j < 10000; j++)
 	{
-		for (long long i = 0; i < 10000000000000000000; i++)
+		for (long long i = 0; i < 10000; i++)
 		{
-			(*m2)(0, 0) = i*j;
-			(*m2)(0, 1) = i;
-			(*m2)(1, 0) = i;
-			(*m2)(1, 1) = i;
+			if (i % 2 == 1)
+			{
+				(*m2)(0, 0) += i*j;
+				(*m2)(0, 1) += i;
+				(*m2)(1, 0) += i;
+				(*m2)(1, 1) += i;
+			}
+			else
+			{
+				(*m2)(0, 0) += j;
+				(*m2)(0, 1) += j;
+				(*m2)(1, 0) += j;
+				(*m2)(1, 1) += j;
+			}
 		}
 	}
 
